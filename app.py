4cc6d1a0ab2c4fbb42420a1d09ba409c9f93cb78
Tabassum <60809283+TabassumShaik@users.noreply.github.com>
2021-04-29 13:32:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import keras
 from PIL import Image, ImageOps
 import numpy as np
 import streamlit as st
-import tensorflow.compat.v1 as tf
+import tensorflow.compat.v2 as tf
 tf.disable_v2_behavior()
 
 st.title("Breast Cancer Prediction")
